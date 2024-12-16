be3856a90fa27c8d969ce45083d0ef244bcf69cb
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2024-12-16 21:19:29 +0300

    Feature Cards animation

--- src/sections/FeaturesCardsSection.tsx
+++ src/sections/FeaturesCardsSection.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 import Card from '../components/Card';
 import TextButton from '../components/TextButton.tsx';
@@ -29,6 +30,20 @@ const cardData = [
   }
 ]
 export default function FeaturesCardsSection() {
+  const [selectedCardIndex, setSelectedCardIndex] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    if (isHovered) return;
+
+    const timeout = setTimeout(
+      () => setSelectedCardIndex((selectedCardIndex + 1) % cardData.length),
+      3000
+    );
+
+    return () => clearTimeout(timeout);
+  }, [selectedCardIndex, isHovered]);
+
   return (
     <section className="py-24 overflow-x-clip md:-mt-28">
       <div className="container mx-auto">
@@ -36,32 +51,47 @@ export default function FeaturesCardsSection() {
 
         <div className="mt-36 flex lg:mt-48">
           <div className="flex flex-none gap-8">
-            {cardData.map(({image, title, description, color}) => (
-              <Card key={title}
-                    color={color}
-                    className="max-w-xs md:max-w-md"
+            {cardData.map(({image, title, description, color}, index) => (
+              <div
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                onClick={() => setSelectedCardIndex(index)}
+                key={title}
+                className="inline-flex transition-all duration-500"
+                style={{
+                  transform: `translateX(calc((-100% - 2rem) * ${selectedCardIndex}))`
+                }}
               >
-                <div className="flex justify-center -mt-28">
-                  <div className="inline-flex relative">
-                    <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
-                    <img src={image}
-                         alt="Pill 3D model"
-                         className="size-40 group-hover:-translate-y-6 transition duration-300"
-                    />
+                <Card
+                  color={color}
+                  className="max-w-xs md:max-w-md"
+                >
+                  <div className="flex justify-center -mt-28">
+                    <div className="inline-flex relative">
+                      <div className="absolute h-4 w-full top-[calc(100%+16px)] bg-zinc-950/70 group-hover:bg-zinc-950/30 transition duration-300 rounded-[100%] [mask-image:radial-gradient(closest-side,black,transparent)]"></div>
+                      <img src={image}
+                           alt="Pill 3D model"
+                           className="size-40 group-hover:-translate-y-6 transition duration-300"
+                      />
+                    </div>
                   </div>
-                </div>
-                <h3 className="font-heading font-black text-3xl mt-12 ">{title}</h3>
-                <p className="text-lg text-zinc-400 mt-4">{description}</p>
-              </Card>
+                  <h3 className="font-heading font-black text-3xl mt-12 ">{title}</h3>
+                  <p className="text-lg text-zinc-400 mt-4">{description}</p>
+                </Card>
+              </div>
             ))}
           </div>
         </div>
 
         <div className="flex justify-center mt-10">
           <div className="bg-zinc-950 inline-flex gap-4 p-2.5 rounded-full">
-            {cardData.map(({title}) => (
+            {cardData.map(({title}, index) => (
               <div key={title}
-                   className="size-2.5 rounded-full bg-zinc-500 cursor-pointer"></div>
+                   className={twMerge(
+                     "size-2.5 rounded-full bg-zinc-500 cursor-pointer",
+                     index === selectedCardIndex && 'bg-zinc-300'
+                   )}
+                   onClick={() => setSelectedCardIndex(index)}></div>
             ))}
           </div>
         </div>
--- src/sections/FeaturesGridSection.tsx
+++ src/sections/FeaturesGridSection.tsx
@@ -1,5 +1,7 @@
+import { useRef } from 'react';
 import CutCornerButton from '../components/CutCornerButton.tsx';
 import TextButton from '../components/TextButton.tsx';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const listItems = [
   'Experience unparalleled security and scalability',
@@ -7,6 +9,43 @@ const listItems = [
   'Unlock the potential of decentralized networks',
 ]
 export default function FeaturesGridSection() {
+  const torusKnotRef = useRef<HTMLImageElement>(null);
+  const firstHemisphereRef = useRef<HTMLImageElement>(null);
+  const coneRef = useRef<HTMLImageElement>(null);
+  const secondHemisphereRef = useRef<HTMLImageElement>(null);
+
+  const {scrollYProgress: torusKnotScrollYProgress} = useScroll({
+    target: torusKnotRef,
+    offset: ["start end", "end start"],
+  })
+
+  const torusKnotTranslateY = useTransform(torusKnotScrollYProgress, [0, 1], [100, -100]);
+  const torusKnotRotate = useTransform(torusKnotScrollYProgress, [0, 1], [30, -30]);
+
+  const {scrollYProgress: firstHemisphereScrollYProgress} = useScroll({
+    target: firstHemisphereRef,
+    offset: ["start end", "end start"],
+  })
+
+  const firstHemisphereTranslateY = useTransform(firstHemisphereScrollYProgress, [0, 1], [50, -50]);
+  const firstHemisphereRotate = useTransform(firstHemisphereScrollYProgress, [0, 1], [-20, -50]);
+
+  const {scrollYProgress: coneScrollYProgress} = useScroll({
+    target: coneRef,
+    offset: ["start end", "end start"],
+  })
+
+  const coneTranslateY = useTransform(coneScrollYProgress, [0, 1], [100, -100]);
+  const coneRotate = useTransform(coneScrollYProgress, [0, 1], [12, 45]);
+
+  const {scrollYProgress: secondHemisphereScrollYProgress} = useScroll({
+    target: secondHemisphereRef,
+    offset: ["start end", "end start"],
+  })
+
+  const secondHemisphereTranslateY = useTransform(secondHemisphereScrollYProgress, [0, 1], [50, -50]);
+  const secondHemisphereRotate = useTransform(secondHemisphereScrollYProgress, [0, 1], [-20, 10]);
+
   return (
     <section className="py-24 overflow-x-clip">
       <div className="container">
@@ -33,13 +72,25 @@ export default function FeaturesGridSection() {
             </div>
             <div className="hidden md:block">
               <div className="relative inline-flex z-0">
-                <img src="/assets/images/torus-knot.png"
-                     alt="Torus Knot 3D model"
-                     className="size-96 max-w-none"
+                <motion.img
+                  ref={firstHemisphereRef}
+                  src="/assets/images/hemisphere.png"
+                  alt="Hemisphere 3D model"
+                  className="absolute size-96 max-w-none  top-3/4 -z-10 scale-x-[-1]"
+                  style={{
+                    translateY: firstHemisphereTranslateY,
+                    rotate: firstHemisphereRotate
+                  }}
                 />
-                <img src="/assets/images/hemisphere.png"
-                     alt="Hemisphere 3D model"
-                     className="absolute size-96 max-w-none  top-3/4 -z-10 scale-x-[-1]"
+                <motion.img
+                  ref={torusKnotRef}
+                  src="/assets/images/torus-knot.png"
+                  alt="Torus Knot 3D model"
+                  className="size-96 max-w-none"
+                  style={{
+                    translateY: torusKnotTranslateY,
+                    rotate: torusKnotRotate
+                  }}
                 />
               </div>
             </div>
@@ -48,28 +99,41 @@ export default function FeaturesGridSection() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div className="relative hidden md:block">
               <div className="absolute right-0 z-0">
-                <img src="/assets/images/cone.png"
-                     alt="Cone 3D model"
-                     className="size-96 max-w-none "
+                <motion.img
+                  ref={coneRef}
+                  src="/assets/images/cone.png"
+                  alt="Cone 3D model"
+                  className="size-96 max-w-none "
+                  style={{
+                    translateY: coneTranslateY,
+                    rotate: coneRotate
+                  }}
                 />
-                <img src="/assets/images/hemisphere.png"
-                     alt="Hemisphere 3D model"
-                     className="absolute size-96 max-w-none  top-3/4 -z-10 right-0 "
+                <motion.img
+                  ref={secondHemisphereRef}
+                  src="/assets/images/hemisphere.png"
+                  alt="Hemisphere 3D model"
+                  className="absolute size-96 max-w-none  top-3/4 -z-10 right-0 "
+                  style={{
+                    translateY: secondHemisphereTranslateY,
+                    rotate: secondHemisphereRotate
+                  }}
                 />
               </div>
             </div>
             <div className="col-span-2">
-            <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl">Blockforge leads the way.</h2>
-            <div className="flex flex-col gap-6 mt-6 text-xl text-zinc-400 lg:text-2xl">
-              <p className="">Blockforge is dedicated to supporting the evolution of Web3 applications by delivering the necessary infrastructure and security for Web3.</p>
-              <p className="">Blockforge champions Web for everyone. As a decentralized blockchain scaling platform, Blockforge enables developers to create
-                              scalable, user-friendly dApps with low transaction costs, all while ensuring robust security.</p>
+              <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl">Blockforge leads the way.</h2>
+              <div className="flex flex-col gap-6 mt-6 text-xl text-zinc-400 lg:text-2xl">
+                <p className="">Blockforge is dedicated to supporting the evolution of Web3 applications by delivering the necessary infrastructure and security for Web3.</p>
+                <p className="">Blockforge champions Web for everyone. As a decentralized blockchain scaling platform, Blockforge enables developers to create
+                                scalable, user-friendly dApps with low transaction costs, all while ensuring robust security.</p>
+              </div>
+              <div className="flex gap-8 mt-12">
+                <CutCornerButton>Get Started</CutCornerButton>
+                <TextButton>Learn More</TextButton>
+              </div>
             </div>
-            <div className="flex gap-8 mt-12">
-              <CutCornerButton>Get Started</CutCornerButton>
-              <TextButton>Learn More</TextButton>
-            </div>
-          </div></div>
+          </div>
         </div>
       </div>
     </section>
